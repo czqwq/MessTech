d15ae4a32934ddd2f3b1dc998aee8db81dc73d70
czqwq <87927909+czqwq@users.noreply.github.com>
2026-07-10 23:14:52 +0800

    Init

---
+++ build.gradle.kts
@@ -0,0 +1,4 @@
+plugins {
+    id("com.github.ElytraServers.elytra-conventions") version "v1.1.2"
+    id("com.gtnewhorizons.gtnhconvention")
+}
---
+++ src/main/java/com/megatech/init/MegaTech.java
@@ -0,0 +1,47 @@
+package com.megatech.init;
+
+import com.megatech.init.Tags;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
+
+@Mod(modid = MegaTech.MODID, version = Tags.VERSION, name = "MegaTech", acceptedMinecraftVersions = "[1.7.10]")
+public class MegaTech {
+
+    public static final String MODID = "MegaTech";
+    public static final Logger LOG = LogManager.getLogger(MODID);
+
+    @SidedProxy(clientSide = "com.megatech.init.ClientProxy", serverSide = "com.megatech.init.CommonProxy")
+    public static CommonProxy proxy;
+
+    @Mod.EventHandler
+    // preInit "Run before anything else. Read your config, create blocks, items, etc, and register them with the
+    // GameRegistry." (Remove if not needed)
+    public void preInit(FMLPreInitializationEvent event) {
+        proxy.preInit(event);
+    }
+
+    @Mod.EventHandler
+    // load "Do your mod setup. Build whatever data structures you care about. Register recipes." (Remove if not needed)
+    public void init(FMLInitializationEvent event) {
+        proxy.init(event);
+    }
+
+    @Mod.EventHandler
+    // postInit "Handle interaction with other mods, complete your setup based on this." (Remove if not needed)
+    public void postInit(FMLPostInitializationEvent event) {
+        proxy.postInit(event);
+    }
+
+    @Mod.EventHandler
+    // register server commands in this event handler (Remove if not needed)
+    public void serverStarting(FMLServerStartingEvent event) {
+        proxy.serverStarting(event);
+    }
+}
